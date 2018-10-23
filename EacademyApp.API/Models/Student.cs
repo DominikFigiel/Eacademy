a0c6e6f23e091f37cddaccdec2d10c7c225e39a4
Radosław Ziemniak <r.ziemniak@outlook.com>
2018-10-23 20:48:40 +0200

    added student and teacher models

--- EacademyApp.API/Models/Student.cs
+++ EacademyApp.API/Models/Student.cs
@@ -2,6 +2,15 @@ namespace EacademyApp.API.Models
 {
     public class Student
     {
-        
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhotoURL { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<CourseStudent> CourseStudents { get; } = new List<CourseStudent>();
     }
 }
--- EacademyApp.API/Models/Teacher.cs
+++ EacademyApp.API/Models/Teacher.cs
@@ -2,6 +2,16 @@ namespace EacademyApp.API.Models
 {
     public class Teacher
     {
-        
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhotoURL { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime HireDate { get; set; }
+        public string Position { get; set; }
+        public ICollection<Course> Courses { get; set; }
     }
 }
