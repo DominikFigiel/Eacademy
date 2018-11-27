0fc2da32721829be9921e3e10c06a5546f8d296e
Martin <haladyn.martin@gmail.com>
2018-11-28 00:33:46 +0100

    Added Pagination Module and Pagination for courses-list module

--- EacademyApp-SPA/src/app/courses/course-list/course-list.component.ts
+++ EacademyApp-SPA/src/app/courses/course-list/course-list.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { PageChangedEvent } from 'ngx-bootstrap';
 import { CourseService } from '../../_services/course.service';
 import { AuthService } from '../../_services/auth.service';
 import { AlertifyService } from '../../_services/alertify.service';
@@ -17,17 +18,31 @@ const httpOptions = {
 })
 export class CourseListComponent implements OnInit {
   courses: any;
+  currentCoursesPageContent: any;
+  itemsPerPage: number;
+  previousPageText: string;
+  nextPageText: string;
 
   constructor(private http: HttpClient, private courseService: CourseService, private authService: AuthService,
     private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.getCourses();
+    this.itemsPerPage = 4;
+    this.previousPageText = 'Poprzednia';
+    this.nextPageText = 'Następna';
+  }
+
+  pageChanged(event: PageChangedEvent): void {
+    const startItem = (event.page - 1) * event.itemsPerPage;
+    const endItem = event.page * event.itemsPerPage;
+    this.currentCoursesPageContent = this.courses.slice(startItem, endItem);
   }
 
   getCourses() {
     this.http.get('http://localhost:5000/api/courses', httpOptions).subscribe(response => {
         this.courses = response;
+        this.currentCoursesPageContent = this.courses.slice(0, 4);
     }, error => {
       console.log(error);
     });
