77bbd282914daa941b615801dd706fe86c27140b
Martin <haladyn.martin@gmail.com>
2018-11-28 00:36:52 +0100

    Added pagination and custom styles for user-course-list

--- EacademyApp-SPA/src/app/students/user-course-list/user-course-list.component.ts
+++ EacademyApp-SPA/src/app/students/user-course-list/user-course-list.component.ts
@@ -9,6 +9,7 @@ import { Observable, of } from 'rxjs';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Course } from '../../_models/course';
+import { PageChangedEvent } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-user-course-list',
@@ -18,16 +19,25 @@ import { Course } from '../../_models/course';
 
 export class UserCourseListComponent implements OnInit {
   student: Student;
+  currentCoursesPageContent: any;
+  itemsPerPage: number;
+  previousPageText: string;
+  nextPageText: string;
+
   constructor(private http: HttpClientModule, private studentService: StudentService, private authService: AuthService,
     private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.loadCourses();
+    this.itemsPerPage = 4;
+    this.previousPageText = 'Poprzednia';
+    this.nextPageText = 'Następna';
   }
   
   loadCourses() {
     this.studentService.getStudent(this.authService.decodedToken.nameid).subscribe((student: Student) => {
       this.student = student;
+      this.currentCoursesPageContent = this.student.courseStudents.slice(0, this.itemsPerPage);
     }, error => {
       this.alertify.error(error);
     });
